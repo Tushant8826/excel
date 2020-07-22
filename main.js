5274e89b4944f240bf6e7309beff69d2844f0767
Tushant8826 <tushantrock841@gmail.com>
2020-07-23 01:51:00 +0530

    electron app

---
+++ main.js
@@ -0,0 +1,22 @@
+const electron =  require("electron");
+const ejs = require("ejs-electron");
+ejs.data ({
+    "title":"My Excel",
+    "rows":100,
+    "cols":26
+})
+const app=electron.app;
+const BrowserWindow = electron.BrowserWindow;
+async function createWindow() {
+    const win=new BrowserWindow({
+        webPreferences:{
+            nodeIntegration:true
+        }
+    })
+    await win.loadFile("index.ejs");
+    win.maximize();
+    win.webContents.openDevTools();
+
+}
+
+app.whenReady().then(createWindow);
